5207b9fee3bd998b68f42737a76c0e613cc310a0
Ibrahim <anceuribrahimelarabi@gmail.com>
2023-08-17 12:18:53 +0100

    add firebase item

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCHY0T0-afZXrkgb1dxjeFuRt7ri-R30jc",
+  authDomain: "ziani-auto.firebaseapp.com",
+  projectId: "ziani-auto",
+  storageBucket: "ziani-auto.appspot.com",
+  messagingSenderId: "1026793267400",
+  appId: "1:1026793267400:web:6ed7de8642896b112485fd",
+  measurementId: "G-YJKLP2J3R9"
+  }
 };
